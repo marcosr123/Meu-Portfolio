b5f85a8e82315e257f55d7bc4f595d6fc13a15ac
Marcos Richard <marcos.richard@hotmail.com>
2021-05-31 20:37:03 -0300

    MUDANÇA DE IMAGENS #13

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,28 @@ function linkAction(){
 }
 
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+//SCROLLREVEAL HOME
+sr.reveal('.home__title',{}); 
+sr.reveal('.button',{delay: 200}); 
+sr.reveal('.home__img',{delay: 400}); 
+sr.reveal('.home__social-icon',{ interval: 200}); 
+//SCROLLREVEAL SOBRE
+sr.reveal('.about__img',{}); 
+sr.reveal('.about__subtitle',{delay: 400}); 
+sr.reveal('.about__text',{delay: 400}); 
+//SCROLLREVEAL SKILLS
+sr.reveal('.skills__subtitle',{}); 
+sr.reveal('.skills__text',{}); 
+sr.reveal('.skills__data',{interval: 200}); 
+sr.reveal('.skills__img',{delay: 600});
+//SCROLLREVEAL WORK
+sr.reveal('.work__img',{interval: 200}); 
+//SCROLLREVEAL CONTATO
+sr.reveal('.contact__input',{interval: 200}); 
